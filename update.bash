99bb34fc02111e57552cce857b8fca7e4fccca69
Karl Semich <0xloem@gmail.com>
2020-10-07 06:40:55 -0400

    git-annex in user@localhost.localdomain:/media/3/TI-calls

--- update.bash
+++ update.bash
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-git annex importfeed --force --fast --template '${feedtitle}/${itempubdate}-${itempubhour}:${itempubminute}:${itempubsecond}-${itemtitle}${extension}' https://www.freeconferencecall.com/rss/podcast?id=a3c83a2ec931a8f5c69c930bfd942aa06f1c02393a36020d9a3fde7a5d9796a4_462001320
+for feed in \
+	https://www.freeconferencecall.com/rss/podcast?id=a3c83a2ec931a8f5c69c930bfd942aa06f1c02393a36020d9a3fde7a5d9796a4_462001320 \
+	https://www.talkshoe.com/rss-pacts-international.xml \
+	https://www.talkshoe.com/rss-pacts-international-conference-calls.xml \
+	https://www.talkshoe.com/rss-targeted-individuals-ffchs.xml \
+	https://www.talkshoe.com/rss-osi-late-night-lounge.xml \
+	https://www.talkshoe.com/rss-electronic-harassmentorganized-stalking.xml \
+	# googling talkshoe and freeconferencecall with reasonable keywords helps find more of these
+; do
+	git annex importfeed --force --fast --template '${feedtitle}/${itempubdate}-${itempubhour}:${itempubminute}:${itempubsecond}-${itemtitle}${extension}' "$feed"
+done
 for file in */*_???
 do
 	name=${file%_???}
